04e6f782953b4aa65fc8743303906404d8721b0d
Bahattin Cinic <bahattin@brickly.io>
2017-05-16 11:26:48 +0300

    add unicode compatible

--- tests/models.py
+++ tests/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.utils.encoding import smart_text, python_2_unicode_compatible
 
 from no_delete.models import NoDeleteModel
 
 
+@python_2_unicode_compatible
 class Supplier(NoDeleteModel):
     name = models.CharField(max_length=255)
 
     default_manager = models.Manager()
 
     def __str__(self):
-        return self.name
+        return smart_text(self.name)
